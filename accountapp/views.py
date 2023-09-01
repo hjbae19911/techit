e244b7ada7e55a98996c3da1f1047f3d9160db92
hjb <hjbae19911@gmail.com>
2023-09-01 15:31:50 +0900

    post request update

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        temp = request.POST.get('email')
+        return render(request, 'accountapp/hello_world.html',
+                      context={"temp": temp})
+
+    temp = "techit"
+    return render(request, 'accountapp/hello_world.html',
+                  context={"temp": temp})
