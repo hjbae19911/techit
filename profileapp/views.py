827699073326a7cb9a145d40898d1c65103d53c5
hjb <hjbae19911@gmail.com>
2023-09-06 10:47:47 +0900

    profile model form update

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileForm
+from profileapp.models import Profile
+
 
 # Create your views here.
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'profileapp/create.html'
+
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk': self.kwargs['pk']})
