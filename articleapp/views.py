a7e8b2ed17b3a96a831a073f2486ec844d2a4b42
hjb <hjbae19911@gmail.com>
2023-09-07 14:14:10 +0900

    articleapp create update

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView, CreateView
+
+from articleapp.forms import ArticleForm
+from articleapp.models import Article
 
 
 # Create your views here.
@@ -7,3 +12,10 @@ from django.views.generic import TemplateView
 
 class TempView(TemplateView):
     template_name = 'articleapp/temp.html'
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'articleapp/create.html'
