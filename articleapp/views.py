e8b625282fe0665fcca4b7ab5d5f966aadd86d39
hjb <hjbae19911@gmail.com>
2023-09-07 11:43:51 +0900

    articleapp tempview update

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+
+class TempView(TemplateView):
+    template_name = 'articleapp/temp.html'
