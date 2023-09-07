4c77b116b98ccaf9a71ff055d59c26d9cdc10db6
hjb <hjbae19911@gmail.com>
2023-09-07 11:15:21 +0900

    profileapp decorator update

--- profileapp/decorators.py
+++ profileapp/decorators.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 
+from profileapp.models import Profile
 
-def account_ownership_required(func):
+
+def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        target_user = User.objects.get(pk=kwargs['pk'])
-        if target_user == request.user:
+        target_profile = Profile.objects.get(pk=kwargs['pk'])
+        if target_profile.user == request.user:
             return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileForm
 from profileapp.models import Profile
 
@@ -23,6 +25,8 @@ class ProfileCreateView(CreateView):
         return reverse('accountapp:detail',
                        kwargs={'pk': self.request.user.pk})
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileForm
